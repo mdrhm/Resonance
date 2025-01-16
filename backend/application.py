e6dd044e9509f2488fbe261f776cbed9594afe28
mdrhm <mdtrahman04@gmail.com>
2025-01-15 22:15:46 -0500

    Added Rating Deletion, Ratings for Pages

--- backend/application.py
+++ backend/application.py
@@ -1,20 +1,29 @@
 from flask import Flask, request, render_template, session, redirect, Response
 from flask_cors import CORS
+import os
+import hashlib
 from queries import SELECT_FROM_WHERE, INSERT_INTO, DELETE_FROM_WHERE, UPDATE_SET_WHERE
 
 app = Flask(__name__)
 CORS(app)
+app.config["SESSION_PERMANENT"] = False
+app.secret_key = os.urandom(24)
 
 @app.route('/ratings', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def ratings():
     match request.method:
         case 'GET':
             user_id = request.args.get('user')
-            songs = "(" + ", ".join(map(lambda x: "'" + x + "'", request.args.get('song').split("-"))) + ")"
+            songs_arr = request.args.get('song').split("-")
+            songs = "(" + ", ".join(map(lambda x: "'" + x + "'", songs_arr)) + ")"
             ratings = SELECT_FROM_WHERE("spotify_id, AVG(rating) as avg_rating, COUNT(rating) as num_of_ratings", "rating", "spotify_id IN " + songs + " GROUP BY spotify_id")
+            songs_with_ratings = list(map(lambda x: x["spotify_id"], ratings))
+            songs_without_ratings = list(filter(lambda x: x not in songs_with_ratings, songs_arr))
+            for song in songs_without_ratings:
+                ratings += [{"spotify_id": song, "rating": "-", "num_of_ratings": 0}]
             for index, rating in enumerate(ratings):
                 user_rating = SELECT_FROM_WHERE("rating", "rating", "user_id = '" + user_id + "' AND spotify_id = '" + rating["spotify_id"] + "'")
-                ratings[index]["user_rating"] = {"rated": "false"} if len(user_rating) == 0 else {"rated": "true", "rating": user_rating[0]["rating"]}
+                ratings[index]["user_rating"] = {"rated": "false"} if not user_rating else {"rated": "true", "rating": user_rating[0]["rating"]}
             return {"ratings": ratings}
         case 'POST':
             user_id = request.args.get('user')
@@ -25,6 +34,14 @@ def ratings():
             rating_inserted = SELECT_FROM_WHERE("spotify_id, AVG(rating) as avg_rating, COUNT(rating) as num_of_ratings", "rating", "spotify_id = '" + song + "' GROUP BY spotify_id")[0]
             rating_inserted["user_rating"] = {"rated": "true", "rating": rating}
             return rating_inserted
+        case 'DELETE':
+            user_id = request.args.get('user')
+            song = request.args.get('song')
+            DELETE_FROM_WHERE("rating", "user_id = '" + user_id + "' AND spotify_id = '" + song + "'")
+            ratings = SELECT_FROM_WHERE("spotify_id, AVG(rating) as avg_rating, COUNT(rating) as num_of_ratings", "rating", "spotify_id = '" + song + "' GROUP BY spotify_id")
+            rating_after_deleted = {"spotify_id": song, "rating": "-", "num_of_ratings": 0} if not ratings else  ratings[0]
+            rating_after_deleted["user_rating"] = {"rated": "false"}
+            return rating_after_deleted
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8000)
--- chrome extension/content.js
+++ chrome extension/content.js
@@ -1,3 +1,5 @@
+let userID
+
 async function getUserID(){
     await document.querySelector('[data-testid="user-widget-link"]').click()
     let userID  = await document.querySelector('[role="menu"]').childNodes[1].querySelector("a").href.split("/").at(-1)
@@ -6,7 +8,7 @@ async function getUserID(){
 }
 
 function getSongIDs() {
-    return Array.from(document.querySelectorAll('[role="row"]:not(.rated) [data-testid="internal-track-link"]')
+    return Array.from(document.querySelectorAll('[role="row"]:not(:has(.rating-container)) [data-testid="internal-track-link"]')
     ).map((song) => song.href.split("/").at(-1))
 }
 
@@ -14,31 +16,31 @@ function markRated() {
     document.querySelectorAll('[role="row"]:not(.rated):has([data-testid="internal-track-link"])').forEach((el) => el.classList.add("rated"))
 }
 
-function getRatings() {
-    let songIDs = getSongIDs().join("-")
-    return getUserID().then(userID => {
-        if(songIDs.length === 0) {
-            return {ratings: []}
-        }
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Access-Control-Allow-Origin', 'https://open.spotify.com');
-        headers.append('Access-Control-Allow-Credentials', 'true');
-
-        return fetch(`https://resonanceapi.pythonanywhere.com/ratings?user=${userID}&song=${songIDs}`, {
-            method: 'GET',
-            headers: headers
+function getRatings(songIDs) {
+    songIDs = songIDs.join("-")
+    if (songIDs.length === 0) {
+        return new Promise((resolve, reject) => {
+            resolve([])
         })
-            .then(response => response.json())  // Parse the JSON response
-            .then(data => {
-                return data;  // Return the JSON data
-            })
-            .catch(error => {
-                console.error('Error fetching ratings:', error);
-                throw error;  // Optionally throw an error if fetch fails
-            });
+    }
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Access-Control-Allow-Origin', 'https://open.spotify.com');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+
+    return fetch(`https://resonanceapi.pythonanywhere.com/ratings?user=${userID}&song=${songIDs}`, {
+        method: 'GET',
+        headers: headers
     })
+        .then(response => response.json())  // Parse the JSON response
+        .then(data => {
+            return data.ratings;  // Return the JSON data
+        })
+        .catch(error => {
+            console.error('Error fetching ratings:', error);
+            throw error;  // Optionally throw an error if fetch fails
+        });
 }
 
 function populateRatings(ratings) {
@@ -52,79 +54,133 @@ function populateRatings(ratings) {
         else if (row.querySelector(".rating-container")) {
             row.querySelector(".rating-container").remove()
         }
-        row.lastChild.innerHTML = generateRating(parseFloat(rating["avg_rating"]).toFixed(1), rating["num_of_ratings"], "4") + row.lastChild.innerHTML
-        addRatingClick(row)
+        row.lastChild.innerHTML = generateRating(rating) + row.lastChild.innerHTML
+        addRatingClick(row.querySelector(".rating-container"))
     }
-
-    let unratedRows = document.querySelectorAll('[data-testid="tracklist-row"]:not(:has(.rating))')
-    for (let row of unratedRows) {
-        row.lastChild.innerHTML = generateRating("-", 0, null) + row.lastChild.innerHTML
-        addRatingClick(row)
-    }
-
 }
 
-function generateRating(avgRating, numberOfRatings, userRating) {
-    return `<div class="rating-container">
+async function populateNowPlaying(){
+    const panelDiv = document.querySelector('[data-testid="NPV_Panel_OpenDiv"] .rating-container')
+    const widgetDiv = document.querySelector('[data-testid="now-playing-widget"] .rating-container')
+
+    if (panelDiv && widgetDiv) {
+        return
+    }
+
+    let nowPlayingID = ''
+    if (!document.querySelector('[data-testid="NPV_Panel_OpenDiv"] [data-testid="context-link"]')) {
+        await document.querySelector('[data-restore-focus-key="now_playing_view"]').click()
+        nowPlayingID = document.querySelector('[data-testid="NPV_Panel_OpenDiv"] [data-testid="context-link"]').href.split("%3A").at(-1)
+        document.querySelector('[data-restore-focus-key="now_playing_view"]').click()
+    }
+    else {
+        nowPlayingID = document.querySelector('[data-testid="NPV_Panel_OpenDiv"] [data-testid="context-link"]').href.split("%3A").at(-1)
+    }
+    getRatings([nowPlayingID]).then(data => {
+        if (panelDiv) {
+            panelDiv.remove()
+        }
+        else if (widgetDiv) {
+            widgetDiv.remove()
+        }
+        let rating = data[0]
+        let ratingDiv = generateRating(rating)
+        if (document.querySelector('[data-testid="NPV_Panel_OpenDiv"] > div')) {
+            document.querySelector('[data-testid="NPV_Panel_OpenDiv"] > div').innerHTML += ratingDiv
+        }
+        document.querySelector('[data-testid="now-playing-widget"]').childNodes[1].innerHTML += ratingDiv
+    })
+}
+
+function populatePageRating(){
+    if (document.querySelector(':has(> [data-testid="entityTitle"]) .rating-container')){
+        return
+    }
+    let pageID = window.location.href.split("/")[4].split("?")[0]
+    getRatings([pageID]).then(data => {
+        let pageRating = data[0]
+        document.querySelector(':has(> [data-testid="entityTitle"])').innerHTML += generateRating(pageRating)
+        addRatingClick(document.querySelector(':has(> [data-testid="entityTitle"]) .rating-container'))
+    })
+}
+
+function generateRating(rating) {
+    let avgRating = (rating["num_of_ratings"] === 0) ? "-" : parseFloat(rating["avg_rating"]).toFixed(1)
+    let numberOfRatings = rating["num_of_ratings"]
+    let userRating = (rating["user_rating"]["rated"]) ? rating["user_rating"]["rating"] : 0
+    let songID = rating["spotify_id"]
+    return `<div class="rating-container" song-id="${songID}">
                 <div class="rating">
                     <span>★</span>
                     ${avgRating}/5 (${numberOfRatings})
                 </div>
-                <div class="give-rating">
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
+                <div class = "ratings">
+                    <div class="give-rating">
+                        <span>☆</span>
+                        <span>☆</span>
+                        <span>☆</span>
+                        <span>☆</span>
+                        <span>☆</span>
+                    </div>
+                    <div class="existing-rating">
+                        <span>${(userRating >= 5) ? `★` : `☆`}</span>
+                        <span>${(userRating >= 4) ? `★` : `☆`}</span>
+                        <span>${(userRating >= 3) ? `★` : `☆`}</span>
+                        <span>${(userRating >= 2) ? `★` : `☆`}</span>
+                        <span>${(userRating >= 1) ? `★` : `☆`}</span>
+                    </div>
                 </div>
            </div>`
 }
 
-function addRatingClick(row) {
-    let ratings = row.querySelectorAll('.give-rating span')
-    for(let i = 0; i < 5; i++){
+function addRatingClick(container) {
+    let ratings = container.querySelectorAll('.give-rating span')
+    let songID = container.getAttribute('song-id')
+    for (let i = 0; i < 5; i++) {
         ratings[i].addEventListener("click", () => {
-            addRating(row.querySelector('a').href.split("/").at(-1), 5 - i)
+            if (!userID) {
+                document.querySelector('[data-testid="login-button"]').click()
+            } else if (Array.from(container.querySelectorAll('.existing-rating span')).filter((star) => {
+                return star.innerHTML === '★'
+            }).length === 5 - i) {
+                updateRating('DELETE', songID, 5 - i)
+            } else {
+                updateRating('POST', songID, 5 - i)
+            }
         })
     }
 }
 
-function addRating(songID, rating) {
+function updateRating(method, songID, rating) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
     headers.append('Access-Control-Allow-Origin', 'https://open.spotify.com');
     headers.append('Access-Control-Allow-Credentials', 'true');
-    return getUserID().then(userID => {
-        return fetch(`https://resonanceapi.pythonanywhere.com/ratings?user=${userID}&song=${songID}&rating=${rating}`, {
-            method: 'POST',
-            headers: headers
-        })
-            .then(response => response.json())
-            .then(data => {
-                return populateRatings([data]);  // Return the JSON data
-            })
-            .catch(error => {
-                console.error('Error fetching ratings:', error);
-                throw error;  // Optionally throw an error if fetch fails
-            });
+    return fetch(`https://resonanceapi.pythonanywhere.com/ratings?user=${userID}&song=${songID}&rating=${rating}`, {
+        method: method,
+        headers: headers
     })
+        .then(response => response.json())
+        .then(data => {
+            return populateRatings([data]);  // Return the JSON data
+        })
+        .catch(error => {
+            console.error('Error fetching ratings:', error);
+            throw error;  // Optionally throw an error if fetch fails
+        })
 }
 
-// setInterval
 setInterval(() => {
-    console.log(getSongIDs())
-    getRatings().then(data => {
-        populateRatings(data.ratings)
-    });
-    markRated()
+    getUserID().then(user => {
+        userID = user
+        getRatings(getSongIDs()).then(data => {
+            populateRatings(data)
+        })
+        populatePageRating()
+        // populateNowPlaying()
+    })
 }, 5000)
-
-window.addEventListener("click", () => {
-    // console.log(getUserID())
-    // console.log(getSongIDs())
-    // markRated()
-})
 
 // MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 //
